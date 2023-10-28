7b35e68e3784fec343aff75f98156bf1c85ba7ae
kailash <kailashrajut0575@gmail.com>
2023-10-28 14:43:32 +0530

    Public , ejs folder in nodejs

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ import path from "path";
 // ejs npm  i ejs
 // 400 bad request, 404 Not found , 200 ok, 500 internal server error
 // send , json ,setheader ,header,statusCode ,sendstatus it,s a express method ,path, set
+// render method used for render   dynamic data
+
 const app = express();
+// 
+// console.log(path.join(path.resolve(),"public"))
+// express.static(path.join(path.resolve(),"public"));
+app.use(express.static(path.join(path.resolve(),"public")));
 app.set("view engine", "ejs")
 app.get("/", (req, res) => {
   // const pathlocation = path.resolve();
 // res.sendFile(path.join(pathlocation, "./index.html"));
 res.render("index", {name :"Abhishek"})
+res.sendFile("index.html")
 });
 
 //
