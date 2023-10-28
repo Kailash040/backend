8b0b93a8ee7d1b7fbe80c557f03b3a8874fef6a7
kailash <kailashrajut0575@gmail.com>
2023-10-28 14:08:28 +0530

    EJS in nodejs

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 console.log("hello ji");
 import express from "express";
 import path from "path";
+// ejs npm  i ejs
 // 400 bad request, 404 Not found , 200 ok, 500 internal server error
-// send , json ,setheader ,header,statusCode ,sendstatus it,s a express method ,path
+// send , json ,setheader ,header,statusCode ,sendstatus it,s a express method ,path, set
 const app = express();
-app.get("/path", (req, res) => {
-  const pathlocation = path.resolve();
-  res.sendFile(path.join(pathlocation, "./index.html"));
+app.set("view engine", "ejs")
+app.get("/", (req, res) => {
+  // const pathlocation = path.resolve();
+// res.sendFile(path.join(pathlocation, "./index.html"));
+res.render("index", {name :"Abhishek"})
 });
 
 //
